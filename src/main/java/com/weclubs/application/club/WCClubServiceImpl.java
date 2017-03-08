de2345db21c1d07937d5fcdc9d42b398d04e6a9a
pandakill <306641225@qq.com>
2017-03-08 20:59:27 +0800

    增加 clubService 服务层

---
+++ src/main/java/com/weclubs/application/club/WCClubServiceImpl.java
@@ -0,0 +1,71 @@
+package com.weclubs.application.club;
+
+import com.weclubs.bean.WCClubBean;
+import com.weclubs.mapper.WCClubMapper;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 社团服务层实现类
+ *
+ * Created by fangzanpan on 2017/3/8.
+ */
+@Service(value = "clubService")
+public class WCClubServiceImpl implements WCIClubService {
+
+    private Logger log = Logger.getLogger(WCClubServiceImpl.class);
+
+    @Autowired
+    private WCClubMapper mClubMapper;
+
+    public WCClubBean getClubInfoById(long clubId) {
+        return null;
+    }
+
+    public void createClub(WCClubBean clubBean) {
+
+        if (clubBean == null) {
+            log.error("createClub：创建 club 对象失败，club 不能为 null。");
+            return;
+        }
+
+        mClubMapper.createClub(clubBean);
+    }
+
+    public void updateClub(WCClubBean clubBean) {
+
+        if (clubBean == null) {
+            log.error("updateClub：更新失败，club 对象不能为空");
+            return;
+        }
+
+        if (clubBean.getId() <= 0) {
+            log.error("updateClub：更新失败，club.id 不能为小于等于 0。");
+        }
+
+        mClubMapper.updateClub(clubBean);
+    }
+
+    public List<WCClubBean> getClubsBySchoolId(long schoolId) {
+
+        if (schoolId <= 0) {
+            log.error("getClubsBySchoolId：schoolId 不能小于等于 0。");
+            return null;
+        }
+
+        return mClubMapper.getClubsBySchoolId(schoolId);
+    }
+
+    public List<WCClubBean> getClubsByStudentId(long studentId) {
+
+        if (studentId <= 0) {
+            log.error("getClubsByStudentId：studentId 不能小于等于 0。");
+            return null;
+        }
+
+        return mClubMapper.getClubsByStudentId(studentId);
+    }
+}
---
+++ src/main/java/com/weclubs/application/club/WCIClubService.java
@@ -0,0 +1,23 @@
+package com.weclubs.application.club;
+
+import com.weclubs.bean.WCClubBean;
+
+import java.util.List;
+
+/**
+ * 社团服务类的接口层
+ *
+ * Created by fangzanpan on 2017/3/8.
+ */
+public interface WCIClubService {
+
+    WCClubBean getClubInfoById(long clubId);
+
+    void createClub(WCClubBean clubBean);
+
+    void updateClub(WCClubBean clubBean);
+
+    List<WCClubBean> getClubsBySchoolId(long schoolId);
+
+    List<WCClubBean> getClubsByStudentId(long studentId);
+}
