8903e63ccd0e154eaf6842cab03c7b6e123ad942
pandakill <306641225@qq.com>
2017-06-21 02:18:30 +0800

    添加融云的 APPkey等

--- src/main/java/com/weclubs/util/Constants.java
+++ src/main/java/com/weclubs/util/Constants.java
@@ -13,8 +13,15 @@ public class Constants {
     public final static String TODO_MEETING = "meeting";
     public final static String TODO_MISSION = "mission";
 
+    // 极光推送的 APP_KEY 和 SECRET_KEY
     public final static String JIGUANG_APP_KEY = "3821df98e100598741efa38d";
     public final static String JIGUANG_SECRET_KEY = "a2f3cd6267bdaf05e8289d34";
+
+    // 融云 IM 的 APP_KEY 和 SECRET_KEY
+    public final static String RONGCLOUD_APP_KEY = "sfci50a7s1xji"; // 测试服务器
+    public final static String RONGCLOUD_SECRET_KEY = "OSe4Pcbk0y"; // 测试服务器
+    private final static String RONGCLOUD_RELEASE_APP_KEY = "sfci50a7s1xji";
+    private final static String RONGCLOUD_RELEASE_SECRET_KEY = "OSe4Pcbk0y";
 
     /* 跳转协议的场景 ID 定义 */
     public final static int SCENE_PERSON_DYNAMIC = 101; // 参与者的动态详情页面
